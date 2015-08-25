cd2032e43b7784e53cb4016bfd50cdf0302c1825
lalalic <lalalic.gmail.com>
2015-08-25 18:42:34 +0800

    auto deploy

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,7 @@ gulp.task('javascript', shell.task('./node_modules/.bin/babel --stage 0 src --ou
     .task('debugTest', shell.task('node --debug-brk=5959 /usr/local/bin/jasmine'))
     .task('default',['run','inspect'])
     .task('mongo.docker', shell.task('docker run --name qili.db -p 27017:27017 -v /data/db:/data/db -d registry.mirrors.aliyuncs.com/library/mongo:3.1  --storageEngine=wiredTiger --directoryperdb'))
-    .task('app.docker', shell.task(['docker build --quiet=true --rm=true --tag="qili"',
-            'docker run --name qili -p 80:9080 -v /data/qili/conf.js:/usr/src/app/conf.js --link qili.db:qili.db -d qili ']))
+    .task('app.docker', shell.task([
+            'docker build --quiet=true --rm=true --tag="qili"',
+            'docker run --name qili -p 9080:9080 -v /data/qili/conf.js:/usr/src/app/conf.js --link qili.db:qili.db -d qili']))
+    .task('nginx.docker', shell.task('docker run --name qili.proxy -v /data/nginx/nginx.conf:/etc/nginx/nginx.conf -p 80:80 --link qili:qili -d registry.mirrors.aliyuncs.com/library/nginx'))
