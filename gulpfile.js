ec98cd00251e37f011e21f0cfd65a5ac69abaf7a
lalalic <lalalic.gmail.com>
2015-09-11 12:21:06 +0800

    auto deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp=require('gulp'),
     shell=require('gulp-shell')
 
 gulp.task('compile', shell.task('./node_modules/.bin/babel --stage 0 src --out-dir dist'))
+    .task('dashboard', function(){
+        require('https').get("https://raw.githubusercontent.com/lalalic/dashboard/master/www/allin1.html",
+            function(response) {
+                response.pipe(require('fs').createWriteStream("www/dashboard/index.html"));
+            });
+    })
     .task('debug', shell.task('node --debug=5858 server.js'))
     .task('inspect', shell.task('node-inspector'))
     .task('debugTest', shell.task('node --debug-brk=5959 /usr/local/bin/jasmine'))
 
-    .task('default',['debug','inspect'])
+    .task('default',['debug','inspect','dashboard'])
 
 
     .task('docker.mongo', shell.task('docker run --name qili.db -p 27017:27017 -v /data/db:/data/db -d mongo  --storageEngine=wiredTiger --directoryperdb'))
