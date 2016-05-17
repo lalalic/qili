02c2894405192f40ac9c826d67b70d59e626bc12
lalalic <lalalic@gmail.com>
2016-05-17 18:42:42 +0800

    auto deploy

---
+++ deploy/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var exec = require('ssh-exec'),
+	env=process.env
+exec('docker-compose restart', {
+	user: env.DEPLOY_USER,
+	host: env.DEPLOY_HOST,
+	password: env.DEPLOY_PASSWORD
+}).pipe(process.stdout)
