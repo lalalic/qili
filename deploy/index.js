56a73210be3ef6b04f5ef1d8cb2c73663bc639cc
lalalic <lalalic@gmail.com>
2016-05-18 11:28:20 +0800

    auto deploy

--- deploy/index.js
+++ deploy/index.js
@@ -2,7 +2,7 @@
 
 var exec = require('ssh-exec'),
 	env=process.env
-exec('IMAGE_ROOT=registry.mirrors.aliyuncs.com/library/ docker-compose restart', {
+exec('cd /data/qili && git pull && cd deploy && IMAGE_ROOT=registry.mirrors.aliyuncs.com/library/ docker-compose restart', {
 	user: env.DEPLOY_USER,
 	host: env.DEPLOY_HOST,
 	password: env.DEPLOY_PASSWORD
