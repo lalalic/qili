6d82aa081f6f32d82195e10b49cf9eccb6cf1d86
lalalic <lalalic@gmail.com>
2016-05-18 13:38:48 +0800

    test travis deploy

--- deploy/index.js
+++ deploy/index.js
@@ -2,7 +2,8 @@
 
 var exec = require('ssh-exec'),
 	env=process.env
-exec('cd /data/qili && git pull && cd deploy && IMAGE_ROOT=registry.mirrors.aliyuncs.com/library/ docker-compose restart', {
+//exec('cd /data/qili && git pull && cd deploy && IMAGE_ROOT=registry.mirrors.aliyuncs.com/library/ docker-compose restart', {
+exec('ls -a',{	
 	user: env.DEPLOY_USER,
 	host: env.DEPLOY_HOST,
 	password: env.DEPLOY_PASSWORD
