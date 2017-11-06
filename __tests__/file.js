abc248f46fa74f77c30fe98fc65ce412ccd12366
Li <lalalic@gmail.com>
2017-11-06 13:56:10 +0800

    optics report

--- __tests__/file.js
+++ __tests__/file.js
@@ -2,7 +2,7 @@ const {resolver}=require("../lib/file")
 
 describe("file",()=>{
 	it("get token", ()=>{
-		let {token,id}=resolver.Mutation.file_token(null,{},{app:{app:{apiKey:"test"}},user:{sessionToken:"456789ghjkl"}})
+		let {token,id}=resolver.Query.file_upload_token(null,{},{app:{app:{apiKey:"test"}},user:{sessionToken:"456789ghjkl"}})
 		expect(token).toBeDefined()
 		expect(id).toBeDefined()
 	})
