a4950442332324c75c31b7eb33a22c5ce8d7f351
lalalic <lalalic@gmail.com>
2019-08-12 18:07:04 +0800

    fix

--- deploy/cert/cert.sh
+++ deploy/cert/cert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /data/certbot:/etc/letsencrypt certbot/dns-aliyun $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
+docker run --rm -v /data/certbot:/etc/letsencrypt certbot/dns-aliyun $1 $2 $3 $4 $5 $6 $7 $8 $9 
