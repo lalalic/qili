553c808988c994c0cd32ac25130ee9de3b4da01b
lalalic <lalalic@139.com>
2020-05-17 18:53:34 +0800

    fix

--- index.js
+++ index.js
@@ -2,42 +2,39 @@ const fs=require("fs"), readFile=fs.readFile
 const cp = require('child_process'), spawn=cp.spawn, exec=cp.exec
 const urllib=require("urllib"), request=urllib.request
 const scheduler=require("node-schedule")
+const querystring = require('querystring');
+
   
 const Qiniu=require("qiniu")
 const config=require("./conf")
 Qiniu.conf.ACCESS_KEY=config.qiniu.ACCESS_KEY
 Qiniu.conf.SECRET_KEY=config.qiniu.SECRET_KEY
-const {conf:{API_HOST,RPC_TIMEOUT}, util}=Qiniu
-  
+const {conf:{API_HOST,RPC_TIMEOUT},rpc, util}=Qiniu
 
 async function syncCert(domain){
-  const code = await renewCert(domain)
-  if(code)
+  const failedRenewCert = await renewCert(domain)
+  if(failedRenewCert)
     return 
+  
   const CertFile=`/data/certbot/live/${domain}/fullchain.pem`
   const PrivFile=`/data/certbot/live/${domain}/privkey.pem`
   const ca=await readMyFile(CertFile)
   const pri=await readMyFile(PrivFile)
   
-  const url=`${API_HOST}/sslcert`
+  const url=`${API_HOST}/sslcert`, content=querystring.stringify({name:domain, ca, pri})
+  console.log(`uploading cert for ${domain}`)
   return new Promise((resolve, reject)=>{
-      request(url,{
-        method:"POST",
-        dataType:"json",
-        contentType:"json",
-        headers:{Authorization:util.generateAccessToken(url)},
-        timeout:RPC_TIMEOUT,
-        data:{name:domain, ca, pri}, 
-      }, 
+      rpc.postWithForm(url,content,util.generateAccessToken(url,content), 
       (err, result, res)=>{
         if(!isError(err, result, res, reject)){
-          updateCert(domain, result.certID).then(resolve, reject)
+          updateCdnCert(domain, result.certID).then(resolve, reject)
         }
       })
   })
 }
 
 function renewCert(domain){
+  console.log(`renewing certifiction for ${domain}`)
   return new Promise((resolve, reject)=>{
     exec(`/data/qili/deploy/cert/cert-renew.sh ${domain}`,{
         cwd:`${__dirname}/deploy/cert`,
@@ -51,16 +48,21 @@ function renewCert(domain){
   })
 }
 
-function updateCert(domain, certId){
+function updateCdnCert(domain, certId){
   console.log(`updating cdn.${domain} cert`)
   return new Promise((resolve, reject)=>{
-    const url=`${API_HOST}/domain/cdn.${domain}/httpsconf`
+    const url=`${API_HOST}/domain/cdn.${domain}/httpsconf`, content=querystring.stringify({certId})
     request(url,{
-      headers:{Authorization:util.generateAccessToken(url)},
-      timeout:RPC_TIMEOUT,
-      contentType:"json",
-      data:{certId},
-    },(err,result, res)=>{
+      method:"PUT",
+      dataType: 'json',
+      timeout: RPC_TIMEOUT,
+      headers:{
+        Authorization:util.generateAccessToken(url,content),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      content,
+    },
+    (err,result, res)=>{
       if(!isError(err, result, res, reject)){
         removeExpiredCerts(domain)
         resolve()
@@ -70,14 +72,14 @@ function updateCert(domain, certId){
 }
 
 function removeExpiredCerts(domain){
-  console.log("cleaning expired certs")
+  console.log(`cleaning expired certs for ${domain}`)
   const url=`${API_HOST}/sslcert`
   request(url,{
     headers:{Authorization:util.generateAccessToken(url)},
     dataType: 'json',
     timeout: RPC_TIMEOUT,
   },(err, result, res)=>{
-    if(!isError(err, result, res, reject) && result && result.certs && result.certs.length){
+    if(!isError(err, result, res, console.error) && result && result.certs && result.certs.length){
       const now=Date.now(), certs=result.certs
       certs.forEach(({not_after,certid, name})=>{
         if(not_after<now && name===domain){
@@ -101,7 +103,7 @@ function readMyFile(path){
   })
 }
 
-function start(){
+function startQili(){
   console.log("starting qili....")
   return new Promise((resolve,reject)=>{
     exec("/root/qili.travis.deploy.sh",(error, stdout, stderr)=>{
@@ -115,7 +117,7 @@ function start(){
 }
 
 async function schedule(){
-  await start()
+  await startQili()
   const url=`${API_HOST}/sslcert`
   request(url,{
     headers:{Authorization:util.generateAccessToken(url)},
@@ -129,7 +131,7 @@ async function schedule(){
       console.log('scheduling cert update for domains: '+domains.join(","))
       const j=scheduler.scheduleJob({start:new Date(not_before-5*24*60*60*1000), rule: '*/3 *'}, ()=>{
           Promise.all(domains.map(domain=>syncCert(domain)))
-            .then(start)
+            .then(startQili)
       })
       if(j){
         console.log('next time on : '+j.nextInvocation())
